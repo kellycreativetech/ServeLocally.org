1dfbb0155401cc8f78daffcddcc1a65254fcfa25
Issac Kelly <issac.kelly@gmail.com>
2011-07-08 17:08:35 -0400

    add favicon into middleware

--- servelocally_project/middleware.py
+++ servelocally_project/middleware.py
@@ -4,3 +4,6 @@ class NoWWW:
     def process_request(self, request):
         if request.get_host() != "servelocally.org":
             return HttpResponseRedirect("http://servelocally.org%s" % request.get_full_path())
+    
+        if request.get_full_path() == "/favicon.ico":
+            return HttpResponseRedirect("/static/img/favicon.ico")
